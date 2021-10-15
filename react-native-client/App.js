730688d9f4761e31ccc0d164526830272db5cd23
RayendraSabandar <rayendratimotius@gmail.com>
2021-10-15 16:35:07 +0700

    added Provider tag and store to App.js

--- react-native-client/App.js
+++ react-native-client/App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Provider } from 'react-redux'
 import { Ionicons } from 'react-native-vector-icons';
 import Detail from './screens/Detail';
 import Home from './screens/Home';
@@ -11,28 +12,31 @@ import About from './screens/About';
 import LandingPage from './screens/LandingPage';
 import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
+import store from './store';
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer >
-        <Stack.Navigator initialRouteName="LandingPage">
-          <Stack.Screen 
-            options={{headerShown: false}}
-            name="LandingPage" 
-            component={LandingPage}/>
-          <Stack.Screen 
-            options={{headerShown: false}}
-            name="SignInScreen" 
-            component={SignIn}/>
-          <Stack.Screen 
-            options={{headerShown: false}}
-            name="SignUpScreen" 
-            component={SignUp}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer >
+          <Stack.Navigator initialRouteName="LandingPage">
+            <Stack.Screen 
+              options={{headerShown: false}}
+              name="LandingPage" 
+              component={LandingPage}/>
+            <Stack.Screen 
+              options={{headerShown: false}}
+              name="SignInScreen" 
+              component={SignIn}/>
+            <Stack.Screen 
+              options={{headerShown: false}}
+              name="SignUpScreen" 
+              component={SignUp}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
